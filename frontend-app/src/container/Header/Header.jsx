95371b3974956cfe6a9fd9d1c1c92497397cda11
Daniel Król <danielkrol90@gmail.com>
2022-08-30 13:26:56 +0200

    created first motion div for header

--- frontend-app/src/container/Header/Header.jsx
+++ frontend-app/src/container/Header/Header.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-
-
-import './Header.scss';
+import { images } from "../../constants";
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="app__header app_flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 2 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>✋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Daniel</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
+        </div>
+      </motion.div>
 
-export default Header
+      <motion.div>
+
+      </motion.div>
+
+      <motion.div>
+        
+      </motion.div>
+
+
+
+
+
+    </div>
+  );
+};
+
+export default Header;
